3a888cf4ceafb7bd3983264be54b9a7a052ec376
BoggyTheSlayer33 <bogdanbogdan147@gmail.com>
2024-10-20 23:36:58 +0300

    Add project files.

---
+++ Coman_Bogdan_Lab2/Data/Coman_Bogdan_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Coman_Bogdan_Lab2.Models;
+
+namespace Coman_Bogdan_Lab2.Data
+{
+    public class Coman_Bogdan_Lab2Context : DbContext
+    {
+        public Coman_Bogdan_Lab2Context (DbContextOptions<Coman_Bogdan_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Coman_Bogdan_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Coman_Bogdan_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
